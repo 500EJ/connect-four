4316d92344f5edf283beddc240f16dee7f4a77bd
Ethan Julius <56324629+500EJ@users.noreply.github.com>
2022-07-21 09:08:36 -0400

    Implement cursor and game logic

--- class/connect-four.js
+++ class/connect-four.js
@@ -2,17 +2,17 @@ const Screen = require("./screen");
 const Cursor = require("./cursor");
 
 class ConnectFour {
-
   constructor() {
-
     this.playerTurn = "O";
 
-    this.grid = [[' ',' ',' ',' ',' ',' ',' '],
-                 [' ',' ',' ',' ',' ',' ',' '],
-                 [' ',' ',' ',' ',' ',' ',' '],
-                 [' ',' ',' ',' ',' ',' ',' '],
-                 [' ',' ',' ',' ',' ',' ',' '],
-                 [' ',' ',' ',' ',' ',' ',' ']]
+    this.grid = [
+      [" ", " ", " ", " ", " ", " ", " "],
+      [" ", " ", " ", " ", " ", " ", " "],
+      [" ", " ", " ", " ", " ", " ", " "],
+      [" ", " ", " ", " ", " ", " ", " "],
+      [" ", " ", " ", " ", " ", " ", " "],
+      [" ", " ", " ", " ", " ", " ", " "]
+    ];
 
     this.cursor = new Cursor(6, 7);
 
@@ -21,7 +21,7 @@ class ConnectFour {
     Screen.setGridlines(true);
 
     // Replace this with real commands
-    Screen.addCommand('t', 'test command (remove)', ConnectFour.testCommand);
+    Screen.addCommand("t", "test command (remove)", ConnectFour.testCommand);
 
     this.cursor.setBackgroundColor();
     Screen.render();
@@ -32,19 +32,97 @@ class ConnectFour {
     console.log("TEST COMMAND");
   }
 
+  static _horizontalWin(grid) {
+    for (let i = 0; i < grid.length; i++) {
+      let lastChar;
+      let count = 1;
+      for (let j = 0; j < grid[i].length; j++) {
+        if (grid[i][j] !== " " && grid[i][j] === lastChar) {
+          count++;
+        } else {
+          lastChar = grid[i][j];
+          count = 1;
+        }
+        if (count === 4) return lastChar;
+      }
+    }
+  }
+
+  static _verticalWin(grid) {
+    const flippedGrid = Array.from(new Array(grid[0].length), () => []);
+    for (let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < grid[i].length; j++) {
+        flippedGrid[j].push(grid[i][j]);
+      }
+    }
+    return ConnectFour._horizontalWin(flippedGrid);
+  }
+
+  static _upwardDiagonalWin(grid) {
+    for (let i = 0; i < grid.length; i++) {
+      for (let j = grid[i].length - 1; j >= 0; j--) {
+        let lastChar = grid[i][j];
+        let count = 1;
+        let [row, col] = [i, j];
+        while (row < grid.length && col >= 0) {
+          if (grid[row][col] === lastChar && grid[row][col] !== " ") {
+            count++;
+          } else {
+            count = 1;
+          }
+          row++;
+          col--;
+        }
+        if (count === 4) return lastChar;
+      }
+    }
+  }
+
+  static _downwardDiagonalWin(grid) {
+    for (let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < grid[i].length; j++) {
+        let lastChar = grid[i][j];
+        let count = 1;
+        let [row, col] = [i, j];
+        while (row < grid.length && col < grid[i].length) {
+          if (grid[row][col] === lastChar && grid[row][col] !== " ") {
+            count++;
+          } else {
+            count = 1;
+          }
+          row++;
+          col++;
+        }
+        if (count === 4) return lastChar;
+      }
+    }
+  }
+
+  static _diagonalWin(grid) {
+    return (
+      ConnectFour._upwardDiagonalWin(grid) ||
+      ConnectFour._downwardDiagonalWin(grid)
+    );
+  }
+
+  static _tie(grid) {
+    return grid.every(row => row.every(char => char !== " ")) && "T";
+  }
+
   static checkWin(grid) {
-
-    // Return 'X' if player X wins
-    // Return 'O' if player O wins
-    // Return 'T' if the game is a tie
-    // Return false if the game has not ended
-
+    return (
+      ConnectFour._horizontalWin(grid) ||
+      ConnectFour._verticalWin(grid) ||
+      ConnectFour._diagonalWin(grid) ||
+      ConnectFour._tie(grid) ||
+      false
+    );
   }
 
   static endGame(winner) {
-    if (winner === 'O' || winner === 'X') {
+    if (winner === "O" || winner === "X") {
       Screen.setMessage(`Player ${winner} wins!`);
-    } else if (winner === 'T') {
+    } else if (winner === "T") {
       Screen.setMessage(`Tie game!`);
     } else {
       Screen.setMessage(`Game Over`);
@@ -52,7 +130,6 @@ class ConnectFour {
     Screen.render();
     Screen.quit();
   }
-
 }
 
 module.exports = ConnectFour;
--- test/cursor-spec.js
+++ test/cursor-spec.js
@@ -1,48 +1,21 @@
-const { expect } = require('chai');
+const { expect } = require("chai");
 
 const Cursor = require("../class/cursor.js");
 const Screen = require("../class/screen.js");
 
-describe ('Cursor', function () {
-
+describe("Cursor", function () {
   let cursor;
 
-  beforeEach(function() {
+  beforeEach(function () {
     cursor = new Cursor(3, 3);
   });
 
-
-  it('initializes for a 3x3 grid', function () {
+  it("initializes for a 3x3 grid", function () {
     expect(cursor.row).to.equal(0);
     expect(cursor.col).to.equal(0);
   });
 
-  it('correctly processes down inputs', function () {
-
-    cursor.down();
-    expect([cursor.row, cursor.col]).to.deep.equal([1, 0]);
-
-    cursor.down();
-    expect([cursor.row, cursor.col]).to.deep.equal([2, 0]);
-
-    cursor.down();
-    expect([cursor.row, cursor.col]).to.deep.equal([2, 0]);
-  });
-
-  it('correctly processes up inputs', function () {
-
-    cursor.up();
-    expect([cursor.row, cursor.col]).to.deep.equal([0, 0]);
-
-    cursor.down();
-    expect([cursor.row, cursor.col]).to.deep.equal([1, 0]);
-
-    cursor.up();
-    expect([cursor.row, cursor.col]).to.deep.equal([0, 0]);
-  });
-
-  it('processes right inputs', function () {
-
+  it("processes right inputs", function () {
     cursor.right();
     expect([cursor.row, cursor.col]).to.deep.equal([0, 1]);
 
@@ -53,8 +26,7 @@ describe ('Cursor', function () {
     expect([cursor.row, cursor.col]).to.deep.equal([0, 2]);
   });
 
-  it('processes left inputs', function () {
-
+  it("processes left inputs", function () {
     cursor.left();
     expect([cursor.row, cursor.col]).to.deep.equal([0, 0]);
 
@@ -64,8 +36,4 @@ describe ('Cursor', function () {
     cursor.left();
     expect([cursor.row, cursor.col]).to.deep.equal([0, 0]);
   });
-
-
-
 });
-
